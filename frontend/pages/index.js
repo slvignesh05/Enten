177122ad35b78b2522c92b4a324f2e67aa7a6b15
slvignesh05 <123097688+slvignesh05@users.noreply.github.com>
2025-06-23 00:09:30 +0530

    Update index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -44,7 +44,7 @@ export default function Home() {
       {/* Animated Blobs */}
       <div className="absolute top-0 left-0 w-full h-full -z-10 overflow-hidden">
         <div className="absolute w-96 h-96 bg-fuchsia-500 rounded-full mix-blend-multiply filter blur-3xl opacity-30 animate-blob"></div>
-        <div className="absolute w-96 h-96 bg-cyan-500 rounded-full mix-blend-multiply filter blur-3xl opacity-30 animate-blob animation-delay-2000 left-1/3"></div>
+        <div className="absolute w-96 h-96 bg-cyan-500 rounded-full mix-blend-multiply filter blur-3xl opacity-30 animate-blob left-1/3" style={{ animationDelay: "2s" }}></div>
         <div className="absolute w-96 h-96 bg-purple-500 rounded-full mix-blend-multiply filter blur-3xl opacity-30 animate-blob animation-delay-4000 top-1/2 left-2/3"></div>
       </div>
 
