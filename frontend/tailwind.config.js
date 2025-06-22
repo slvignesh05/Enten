2523a9a922326c56835d86b804f3496706fef2d0
slvignesh05 <123097688+slvignesh05@users.noreply.github.com>
2025-06-22 23:55:08 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,20 +1,24 @@
-theme: {
-  extend: {
-    animation: {
-      blob: "blob 7s infinite",
-      fade-in: "fadeIn 1s ease-in-out",
-    },
-    keyframes: {
-      blob: {
-        "0%": { transform: "translate(0px, 0px) scale(1)" },
-        "33%": { transform: "translate(30px, -50px) scale(1.1)" },
-        "66%": { transform: "translate(-20px, 20px) scale(0.9)" },
-        "100%": { transform: "translate(0px, 0px) scale(1)" },
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      animation: {
+        blob: "blob 7s infinite",
+        "fade-in": "fadeIn 1s ease-in forwards",
       },
-      fadeIn: {
-        "0%": { opacity: 0 },
-        "100%": { opacity: 1 },
+      keyframes: {
+        blob: {
+          "0%, 100%": { transform: "translate(0px, 0px) scale(1)" },
+          "33%": { transform: "translate(30px, -20px) scale(1.1)" },
+          "66%": { transform: "translate(-20px, 20px) scale(0.9)" },
+        },
+        fadeIn: {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        },
       },
     },
   },
-}
+  plugins: [],
+};
