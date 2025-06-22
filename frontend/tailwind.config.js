66c24b6d291dccd4ef9e25f644ce7117a2c217d6
slvignesh05 <123097688+slvignesh05@users.noreply.github.com>
2025-06-22 23:59:09 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,21 +1,22 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       animation: {
         blob: "blob 7s infinite",
-        "fade-in": "fadeIn 1s ease-in forwards",
+        fade: "fadeIn 1s ease-in forwards",
       },
       keyframes: {
         blob: {
-          "0%, 100%": { transform: "translate(0px, 0px) scale(1)" },
-          "33%": { transform: "translate(30px, -20px) scale(1.1)" },
+          "0%": { transform: "translate(0px, 0px) scale(1)" },
+          "33%": { transform: "translate(30px, -50px) scale(1.1)" },
           "66%": { transform: "translate(-20px, 20px) scale(0.9)" },
+          "100%": { transform: "translate(0px, 0px) scale(1)" },
         },
         fadeIn: {
-          from: { opacity: 0 },
-          to: { opacity: 1 },
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
         },
       },
     },
